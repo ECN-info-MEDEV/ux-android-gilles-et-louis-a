84f773b77b7b1ac5f84ee542db64e30271af070c
Nartrack <71267249+Nartrack@users.noreply.github.com>
2022-03-24 12:00:13 +0100

    rendre le lien cliquable

--- app/src/main/java/com/example/jobscompare/JobOffer.java
+++ app/src/main/java/com/example/jobscompare/JobOffer.java
@@ -1,12 +1,19 @@
 package com.example.jobscompare;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.text.style.URLSpan;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class JobOffer extends AppCompatActivity {
     private static final String TAG = "Activity-JobOffer";
@@ -31,9 +38,23 @@ public class JobOffer extends AppCompatActivity {
 
         //retrieving user input from NewOffer Activity
         String link_input = Extra.getStringExtra("User_link_input");
+        if (!link_input.startsWith("http://") && !link_input.startsWith("https://"))
+            link_input = "http://" + link_input;
+        SpannableString clicable_link = new SpannableString("lien de l’offre");
+        clicable_link.setSpan(new URLSpan(link_input), 0, 15, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
         Log.i(TAG, "link_input : "+link_input);
         TextView mText3 = (TextView) findViewById(R.id.link_input);
-        mText3.setText(link_input);
+        mText3.setText(clicable_link);
+
+        ClickableSpan cs = new ClickableSpan() {
+            @Override
+            public void onClick(View activity_job_offer) {
+                Toast.makeText(getApplicationContext(), "this was clicked", Toast.LENGTH_LONG).show();
+            }
+        };
+
+        mText3.setMovementMethod(LinkMovementMethod.getInstance());
+
 
         /*//retrieving user input from NewOffer Activity
         String address_input = Extra.getStringExtra("User_address_input");
