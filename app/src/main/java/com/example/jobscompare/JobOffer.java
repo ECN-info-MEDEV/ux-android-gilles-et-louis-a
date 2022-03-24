9a0ee2c0a97bab14930a244404eb9174ac3d5399
genvox <louis.augereau@eleves.ec-nantes.fr>
2022-03-24 01:01:16 +0100

    correction de l'erreur sur les values.xml et passage de messages multiples entre l'activité NewOffer et JobOffer

--- app/src/main/java/com/example/jobscompare/JobOffer.java
+++ app/src/main/java/com/example/jobscompare/JobOffer.java
@@ -4,10 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 public class JobOffer extends AppCompatActivity {
+    private static final String TAG = "Activity-JobOffer";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +19,21 @@ public class JobOffer extends AppCompatActivity {
         //retrieving user input from NewOffer Activity
         Intent Extra = getIntent();
         String user_entreprise_input = Extra.getStringExtra("User_entreprise_Input");
-
+        Log.i(TAG, "user_entreprise_input : "+user_entreprise_input);
         TextView mText = (TextView) findViewById(R.id.OfferCompagnyName);
         mText.setText(user_entreprise_input);
+
+        //retrieving user input from NewOffer Activity
+        String text_poste = Extra.getStringExtra("User_poste_input");
+        Log.i(TAG, "text_poste : "+text_poste);
+        TextView mText2 = (TextView) findViewById(R.id.text_poste);
+        mText2.setText(text_poste);
+
+        //retrieving user input from NewOffer Activity
+        String link_input = Extra.getStringExtra("User_link_input");
+        Log.i(TAG, "link_input : "+link_input);
+        TextView mText3 = (TextView) findViewById(R.id.link_input);
+        mText3.setText(link_input);
 
 
     }
--- app/src/main/java/com/example/jobscompare/NewOffer.java
+++ app/src/main/java/com/example/jobscompare/NewOffer.java
@@ -3,7 +3,9 @@ package com.example.jobscompare;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,7 +15,13 @@ import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class NewOffer extends AppCompatActivity {
+
+    private static final String TAG = "Activity-NewOffer";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,9 +37,33 @@ public class NewOffer extends AppCompatActivity {
                 //Sending the message
                 Intent sendMessage = new Intent(NewOffer.this,  JobOffer.class);
                 sendMessage.putExtra("User_entreprise_Input", entreprise_name);
+                Log.i(TAG, "entreprise_name : "+entreprise_name);
+
+
+                //TODO : create a function that gets the user input and sends the message
+
+                //TODO : add the address field in the NewOffer Activity
+
+                //the following block iterate through ids rather than copy-pasting the previous
+                //lines of code
+                Resources r = getResources();
+                String name = getPackageName();
+                List<String> ids_name = Arrays.asList("poste_input", "link_input");
+                String name_placeholder;
+                for (String id_name : ids_name) {
+                    //getting user input using the id of the text input
+                    EditText mEdit2 = (EditText) findViewById(r.getIdentifier(id_name, "id", name));
+                    Log.i(TAG, "id_name : "+id_name);
+                    Log.i(TAG, "id : "+r.getIdentifier(id_name, "id", name));
+                    name_placeholder = mEdit2.getText().toString();
+
+                    //Sending the message
+                    sendMessage.putExtra("User_"+id_name, name_placeholder);
+                    Log.i(TAG, "name_placeholder : "+name_placeholder);
+
+                }
+
                 startActivity(sendMessage);
-                //Nb : the message is not persistent. if we go back to input screen
-                // it replaces the message with blank
             }
         });
     }
